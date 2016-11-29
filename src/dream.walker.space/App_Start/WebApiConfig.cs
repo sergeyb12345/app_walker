ca78080cb1d112a67d1cc772dbaeeadf38b0c6ca
sergey.balaboskin <sergey.balaboskin@just-eat.com>
2016-11-29 17:40:21 +0000

    refactor

--- src/dream.walker.space/App_Start/WebApiConfig.cs
+++ src/dream.walker.space/App_Start/WebApiConfig.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Filters;
 using Autofac.Integration.WebApi;
+using dream.walker.space.Filters;
 using dream.walker.space.IoC;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -34,50 +28,6 @@ namespace dream.walker.space
             var settings = config.Formatters.JsonFormatter.SerializerSettings;
             settings.Formatting = Formatting.Indented;
             settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-        }
-    }
-
-    public class NoStackExceptionFilterAttribute : ExceptionFilterAttribute
-    {
-        public override void OnException(HttpActionExecutedContext context)
-        {
-
-            if (context.Exception is NotImplementedException)
-            {
-                context.Response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
-                return;
-            }
-
-            var exception = context.Exception;
-            var message = new StringBuilder();
-            var source = new StringBuilder(context.Request.RequestUri.PathAndQuery);
-
-            source.Append($" => {exception.Source}");
-            message.AppendLine($"{exception.Message}. ");
-
-            while (exception.InnerException != null)
-            {
-                message.AppendLine($"{exception.InnerException.Message}. ");
-                source.Append($" => {exception.InnerException.Source}");
-
-                exception = exception.InnerException;
-            }
-            var error = new
-            {
-                Source = source.ToString(),
-                Message = message.ToString()
-            };
-
-            context.Response = new HttpResponseMessage
-            {
-                Content = new StringContent( JsonConvert.SerializeObject(error), Encoding.UTF8, "application/json"),
-                StatusCode = System.Net.HttpStatusCode.InternalServerError
-            };
-        }
-
-        public override Task OnExceptionAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
-        {
-            return base.OnExceptionAsync(actionExecutedContext, cancellationToken);
         }
     }
 }
---
+++ src/dream.walker.space/Filters/NoStackExceptionFilterAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using Newtonsoft.Json;
+
+namespace dream.walker.space.Filters
+{
+    public class NoStackExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+
+            if (context.Exception is NotImplementedException)
+            {
+                context.Response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+                return;
+            }
+
+            var exception = context.Exception;
+            var message = new StringBuilder();
+            var source = new StringBuilder(context.Request.RequestUri.PathAndQuery);
+
+            source.Append($" => {exception.Source}");
+            message.AppendLine($"{exception.Message}. ");
+
+            while (exception.InnerException != null)
+            {
+                message.AppendLine($"{exception.InnerException.Message}. ");
+                source.Append($" => {exception.InnerException.Source}");
+
+                exception = exception.InnerException;
+            }
+            var error = new
+            {
+                Source = source.ToString(),
+                Message = message.ToString()
+            };
+
+            context.Response = new HttpResponseMessage
+            {
+                Content = new StringContent( JsonConvert.SerializeObject(error), Encoding.UTF8, "application/json"),
+                StatusCode = System.Net.HttpStatusCode.InternalServerError
+            };
+        }
+
+        public override Task OnExceptionAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            return base.OnExceptionAsync(actionExecutedContext, cancellationToken);
+        }
+    }
+}
