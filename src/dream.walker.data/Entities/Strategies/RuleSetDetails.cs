e361cffa34f1da5770ba780786d034147c383de2
sergey.balaboskin <sergey.balaboskin@just-eat.com>
2016-12-05 12:27:04 +0000

    rulesetdetails

--- src/dream.walker.data/Entities/Strategies/RuleSetDetails.cs
+++ src/dream.walker.data/Entities/Strategies/RuleSetDetails.cs
@@ -1,10 +1,17 @@
-﻿namespace dream.walker.data.Entities.Strategies
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dream.walker.data.Entities.Strategies
 {
     public class RuleSetDetails
     {
-        public int Id { get; set; }
         public int RuleSetId { get; set; }
         public int RuleId { get; set; }
         public bool Deleted { get; set; }
+
+        [ForeignKey("RuleSetId")]
+        public virtual RuleSet RuleSet { get; set; }
+
+        [ForeignKey("RuleId")]
+        public virtual Rule Rule { get; set; }
     }
 }
