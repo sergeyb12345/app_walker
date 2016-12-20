45f0b10b339208b71ca8eff4fb53777bb9f17e68
sergeyb12345 <sbalaboskin@gmail.com>
2016-12-20 13:09:03 +0000

    default params

--- src/dream.walker.space/src/resources/elements/indicator/indicator.js
+++ src/dream.walker.space/src/resources/elements/indicator/indicator.js
@@ -23,7 +23,21 @@ export class Indicator {
         this.subscriptions = [];
         this.indicatorDataSeries = [];
         this.periods = this.globalSettings.periods;
-        this.formulaes = ['EMA','MACD','ForceIndex']
+        
+        this.formulaes = [
+            {name:'EMA', defaults: [
+                    {paramName:'Period', value: 13}
+            ]},
+            {name:'MACD', defaults: [
+                {paramName:'FastEmaPeriod', value: 12},
+                {paramName:'SlowEmaPeriod', value: 26},
+                {paramName:'SignalEmaPeriod', value: 9}
+            ]},
+            {name:'ForceIndex', defaults: [
+                    {paramName:'Period', value: 13}
+            ]}
+        ];
+
         this.plotNumbers = [0,1,2,3];
         this.chartTypes = [
             {name:'Ohlc',id: 0},
@@ -48,13 +62,22 @@ export class Indicator {
         }
     }
 
+    onFormulaChange() {
+        let defParams = this.formulaes.filter(c => c.name === this.indicatorInfo.name);
+        if(defParams && defParams.length > 0) {
+            this.indicatorInfo.params = defParams[0].defaults;
+        } else {
+            toastr.warning('Unable to pull default params for selected formula.', 'Data is missing');
+        }
+    }
+
     startEdit() {
         this.originalIndicator = Object.assign({}, this.indicatorInfo);
         this.indicatorInfo.editMode = true;
 
         ValidationRules
-            .ensure(u => u.description).displayName('Indicator Name').required().withMessage(`\${$displayName} cannot be blank.`)
-            .ensure(u => u.chartColor).displayName('Line Color').required().withMessage(`\${$displayName} cannot be blank.`)
+            .ensure('description').displayName('Indicator Name').required().withMessage(`\${$displayName} cannot be blank.`)
+            .ensure('chartColor').displayName('Line Color').required().withMessage(`\${$displayName} cannot be blank.`)
                 .matches(/^#[0-9A-F]{6}$/).withMessage(`\${$displayName} value should be in format: #AAFF99.`)
             .on(this.indicatorInfo);
 
