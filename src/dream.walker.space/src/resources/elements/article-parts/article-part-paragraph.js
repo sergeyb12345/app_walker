a1b5fcc8d1999fbe5ac3c632ded9c4e01c579fe4
sergeyb12345 <sbalaboskin@gmail.com>
2016-12-26 19:10:37 +0000

    paragraph-validation

--- src/dream.walker.space/src/resources/elements/article-parts/article-part-paragraph.js
+++ src/dream.walker.space/src/resources/elements/article-parts/article-part-paragraph.js
@@ -1,8 +1,43 @@
-﻿import {inject, bindable} from "aurelia-framework";
+﻿import {inject, bindable, BindingEngine} from "aurelia-framework";
 
+@inject(BindingEngine)
 export class ArticlePartParagraph {
     @bindable part;
 
-    constructor () {
+    constructor (bindingEngine) {
+        this.bindingEngine = bindingEngine;
+        this.subscriptions = [];
+
     }
+
+    attached() {
+        if (!this.part.text) {
+            this.part.text = '';
+        }
+
+        this.subscriptions.push(this.bindingEngine.propertyObserver(this.part, 'text')
+            .subscribe((newValue, oldValue) => this.onChange()));
+
+        this.validate();
+    }
+
+    detached() {
+        if (this.subscriptions.length > 0) {
+            this.subscriptions.forEach(function(subscription) {
+                subscription.dispose();
+            });
+        }
+
+    }
+
+    onChange() {
+        this.validate();
+    }    
+    
+    validate() {
+        this.textValid = this.part.text.length > 0;
+
+        this.part.valid = this.textValid;
+    }
+
 }
