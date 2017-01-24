e55aa29b6f7312342c68fdf745ae82bb758244cf
sergey.balaboskin <sergey.balaboskin@just-eat.com>
2017-01-24 16:55:20 +0000

    progress calc

--- src/dream.walker.playground/Models/StrategyRuleSetResult.cs
+++ src/dream.walker.playground/Models/StrategyRuleSetResult.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace dream.walker.playground.Models
 {
@@ -15,12 +17,16 @@ namespace dream.walker.playground.Models
 
         public int RuleSetId { get; private set; }
         public string Name { get; private set; }
+        public int Progress { get; private set; }
         public List<StrategyRuleResult> Rules { get; private set; }
 
 
         public void Add(StrategyRuleResult rule)
         {
             Rules.Add(rule);
+
+            var totalValid = Rules.Count(r => r.Valid) * 1.0;
+            Progress = (int)(totalValid / Rules.Count * 100);
         }
     }
 }
